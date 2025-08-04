8acdd138d34f225933de03287aadab6f529a8ca2
Raja-Nalliyappan <n.raja3710@gmail.com>
2025-08-04 13:48:36 +0530

    expriance color change

--- src/Portfoilo/Experience.jsx
+++ src/Portfoilo/Experience.jsx
@@ -1,11 +1,10 @@
-import { Footer } from "./Footer"
 import { Navbar } from "./Navbar"
 
 export const Experience =()=>{
     return(
         <>
         <Navbar />
-            <h1>Hi this is experience page</h1>
+            <h1 style={{color:"white"}}>Hi this is experience page</h1>
         </>
     )
 }
