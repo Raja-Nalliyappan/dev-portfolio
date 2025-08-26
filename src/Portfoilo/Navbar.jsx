ad2d27a5a020b391699583a8f59fc96b4ea6b928
Raja-Nalliyappan <n.raja3710@gmail.com>
2025-08-26 14:58:00 +0530

    new imp

--- src/Portfoilo/Navbar.jsx
+++ src/Portfoilo/Navbar.jsx
@@ -2,7 +2,7 @@ import instagramIcon from '../assets/instagram.svg';
 import linkedinIcon from '../assets/linkedin.svg';
 import githubIcon from '../assets/github.svg';
 import whatsappIcon from '../assets/whatsapp.svg';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import "./Navbar.css"
 
 export const Navbar =()=>{
@@ -12,17 +12,17 @@ export const Navbar =()=>{
                 <div className='main-header'>
                     <nav className="navbar-card">
                         <ul className="nav-list">
-                            <li><Link to="/home">Home</Link></li>
-                            <li><Link to="/experience">Experience</Link></li>
-                            <li><a href="#">Projects</a></li>
-                            <li><a href="">Recent work</a></li>
-                            <li><Link to="/skills">Skills</Link></li>
+                            <li><NavLink to="/home" style={({isActive})=>isActive?{backgroundColor:"#829ecaff",borderRadius:"10px"}:{color:"white"}}>Home</NavLink></li>
+                            <li><NavLink to="/experience" style={({isActive})=>isActive?{backgroundColor:"#829ecaff",borderRadius:"10px"}:{color:"white"}}>Experience</NavLink></li>
+                            <li><NavLink to="/projects" style={({isActive})=>isActive?{backgroundColor:"#829ecaff",borderRadius:"10px"}:{color:"white"}}>Projects</NavLink></li>
+                            <li><NavLink to="/recentwork" style={({isActive})=>isActive?{backgroundColor:"#829ecaff",borderRadius:"10px"}:{color:"white"}}>Recent work</NavLink></li>
+                            <li><NavLink to="/skills" style={({isActive})=>isActive?{backgroundColor:"#829ecaff",borderRadius:"10px"}:{color:"white"}}>Skills</NavLink></li>
                         </ul>
                     </nav>
                     <nav className="navbar-img">
                         <ul className="nav-bar-img-list">
                             <li><a href="#" target='_blank' rel='oopener noreferrer'><img src={instagramIcon} alt="instagram" width="35" height="35"/></a></li>
-                            <li><a href="#" target='_blank' rel='oopener noreferrer'><img src={linkedinIcon} alt="linkedin" width="35" height="35"/></a></li>
+                            <li><a href="https://www.linkedin.com/in/raja-nalliyappan-22a131174/" target='_blank' rel='oopener noreferrer'><img src={linkedinIcon} alt="linkedin" width="35" height="35"/></a></li>
                             <li><a href="https://github.com/Raja-Nalliyappan" target='_blank' rel='oopener noreferrer'><img src={githubIcon} alt="" width="35" height="35"/></a></li>
                             <li><a href="https://wa.me/9626945746?text=Hello%20I%20saw%20your%20portfolio!" target='_blank' rel='oopener noreferrer'><img src={whatsappIcon} alt="" width="31" height="31" style={{marginBottom:"2px"}}/></a></li>
                         </ul>
@@ -32,9 +32,3 @@ export const Navbar =()=>{
         </div>
     )
 }
-
-                    
-                    
-                    
-                    
-                    
--- src/Portfoilo/Skills.jsx
+++ src/Portfoilo/Skills.jsx
@@ -2,9 +2,12 @@ import html from "../assets/html-5.svg"
 import css from "../assets/CSS-3.svg"
 import js from "../assets/javascript.svg"
 import react from "../assets/react-js.svg"
+import csharo from "../assets/Csharp.svg"
 import { Navbar } from "./Navbar"
 import "./Skills.css"
 import { useEffect } from "react"
+import { Footer } from "./Footer"
+
 export const Skills = ()=>{
     useEffect(()=>{
         document.body.style.backgroundColor="black"
@@ -12,15 +15,18 @@ export const Skills = ()=>{
     return(
         <>
         <Navbar />
-            <h1 style={{color:"white", }}>Development skills</h1>
+            <h1 style={{color:"white", display:"flex", justifyContent:"center", alignItems:"center", margin:"20px", fontFamily: "system-ui, sans-serif"}}>Development skills</h1>
+            <p style={{color:"white", display:"flex", justifyContent:"center", alignItems:"center", margin:"20px", }}>Full Stack .NET Developer skilled in ASP.NET Core, Web API, React.js, and SQL Server with strong OOPs and problem-solving skills.</p>
             <div className="skill-scroll-wrapper">
                 <div className="skill-list">
                     <img src={html} alt="html" />
                     <img src={css} alt="css" />
                     <img src={js} alt="js" />
                     <img src={react} alt="react" className="react-logo"/>
+                    <img src={csharo} alt="CSharp"/>
                 </div>
             </div>
+        <Footer />
         </>
     )
 }
