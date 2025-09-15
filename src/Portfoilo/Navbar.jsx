1919bf6a96200cb5b15de58e32363fe640b5bd9a
Raja-Nalliyappan <n.raja3710@gmail.com>
2025-09-15 22:26:58 +0530

    test

--- src/Portfoilo/Navbar.jsx
+++ src/Portfoilo/Navbar.jsx
@@ -8,15 +8,6 @@ import "./Navbar.css"
 
 export const Navbar =()=>{
 
-    // const resumeDownload =()=>{
-    //     const link = document.createElement("a");
-    //     link.href = "/Resume/Raja.pdf";
-    //     link.download = "Raja_resume.pdf";
-    //     document.body.appendChild(link);
-    //     link.click();
-    //     document.body.removeChild(link);
-    // }
-
     return(
         <div>
             <header>
@@ -26,7 +17,6 @@ export const Navbar =()=>{
                             <li><NavLink to="/home" style={({isActive})=>isActive?{backgroundColor:"#829ecaff",borderRadius:"10px"}:{color:"white"}}>Home</NavLink></li>
                             <li><NavLink to="/experience" style={({isActive})=>isActive?{backgroundColor:"#829ecaff",borderRadius:"10px"}:{color:"white"}}>Experience</NavLink></li>
                             <li><NavLink to="/projects" style={({isActive})=>isActive?{backgroundColor:"#829ecaff",borderRadius:"10px"}:{color:"white"}}>Projects</NavLink></li>
-                            {/* <li><NavLink to="/recentwork" style={({isActive})=>isActive?{backgroundColor:"#829ecaff",borderRadius:"10px"}:{color:"white"}}>Recent work</NavLink></li> */}
                             <li><NavLink to="/skills" style={({isActive})=>isActive?{backgroundColor:"#829ecaff",borderRadius:"10px"}:{color:"white"}}>Skills</NavLink></li>
                         </ul>
                     </nav>
@@ -36,9 +26,8 @@ export const Navbar =()=>{
                             <li><a href="https://www.linkedin.com/in/raja-nalliyappan-22a131174/" target='_blank' rel='oopener noreferrer'><img src={linkedinIcon} alt="linkedin" width="35" height="35"/></a></li>
                             <li><a href="https://github.com/Raja-Nalliyappan" target='_blank' rel='oopener noreferrer'><img src={githubIcon} alt="" width="35" height="35"/></a></li>
                             <li><a href="https://wa.me/9626945746?text=Hi%2C%20I%E2%80%99m%20interested%20in%20your%20work." target='_blank' rel='oopener noreferrer'><img src={whatsappIcon} alt="" width="31" height="31" style={{marginBottom:"2px"}}/></a></li>
-                            {/* <li><button onClick={resumeDownload} style={{position:"relative", left:"50px" ,bottom:"4px", cursor: "pointer"}} ><img src={download} alt="" width="25" height="25"/></button></li> */}
                         </ul>
-                    </nav>
+                     </nav>
                 </div>
             </header>
         </div>
